f8e8e8b28dc64ef416da62fc97bccbe2b8f66305
Angelo Teixeira <imnotteixeira@gmail.com>
2020-10-17 14:21:23 +0100

    Fetched publishers for each game - sparql fix - not using wikipedia api

--- src/main.py
+++ src/main.py
@@ -4,46 +4,45 @@ from qwikidata.sparql  import return_sparql_query_results
 
 df = pd.read_csv ("test.csv")
 
+session = requests.Session()
+
 def query(title):
 
-    session = requests.Session()
+    print("querying: ", title)
+    # query_string = f"""
+    #     PREFIX schema: <http://schema.org/>
+    #     SELECT ?title ?publisher ?publisherLabel $article WHERE {{
+    #         ?item wdt:P31 wd:Q7889 .
+    #         ?item rdfs:label "{title}"@en
+    #         OPTIONAL {{ ?item wdt:P123 ?publisher. }}
+    #         OPTIONAL {{ 
+    #             ?article schema:about ?publisher .
+    #             ?article schema:inLanguage "en" .
+    #             ?article schema:isPartOf <https://en.wikipedia.org/> . 
+    #         }}
+    #     }}    
+    # """
+    query_string = f"""
+        PREFIX schema: <http://schema.org/>
+        SELECT ?title ?publisher WHERE {{
+            ?item wdt:P31 wd:Q7889 .
+            ?item rdfs:label "{title}"@en
+            OPTIONAL {{ ?item wdt:P123 ?publisher. }}
+        }}    
+    """
 
-    url = "https://en.wikipedia.org/w/api.php"
-    params = {
-        "action": "query",
-        "format": "json",
-        "prop": "pageprops",
-        "titles": title,
-    }
+    res = return_sparql_query_results(query_string)
+    print(res)
 
-    response = session.get(url=url, params=params)
-    data = response.json()
-    pages = data["query"]["pages"]
-
-    for key, val in pages.items():
-        query_string = f"""
-            PREFIX schema: <http://schema.org/>
-            SELECT ?title ?publisher ?publisherLabel $article WHERE {{
-                wd:{val['pageprops']['wikibase_item']} wdt:P123 ?publisher.
-                OPTIONAL {{ wd:{val['pageprops']['wikibase_item']} wdt:P1476 ?title. }}
-                OPTIONAL {{ 
-                    ?article schema:about ?publisher .
-                    ?article schema:inLanguage "en" .
-                    ?article schema:isPartOf <https://en.wikipedia.org/> . 
-                }}
-            }}    
-        """
-
-        res = return_sparql_query_results(query_string)
-
-        for row in res["results"]["bindings"]:
-            print("Publisher:", row['article']['value'][30:])
+    for row in res["results"]["bindings"]:
+        # print("Publisher:", row['article']['value'][30:])
+        print("Publisher:", row['publisher'])
 
     
 
 def myFunc(names):
-    print(names)
     for name in names:
+        print(name)
         query(name)
 
 
